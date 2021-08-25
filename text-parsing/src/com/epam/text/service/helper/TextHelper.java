bda522212ae8022b0906c3583cb755ff915d754a
Pavel Sirotsin <pavel.sirotsin@gmail.com>
2021-08-25 17:17:08 +0300

    implemented TextHelper.class , refactored TextService.class

---
+++ text-parsing/src/com/epam/text/service/helper/TextHelper.java
@@ -0,0 +1,47 @@
+package com.epam.text.service.helper;
+
+import com.epam.text.entity.Paragraph;
+import com.epam.text.entity.Sentence;
+import com.epam.text.entity.Text;
+import com.epam.text.entity.Word;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TextHelper {
+    public static void putInOrderAfterSort(Text workSample, List<Sentence> allInOne) {
+        int fromIndex = 0;
+        int toIndex;
+        List<Sentence> sublist;
+        for (Paragraph paragraph : workSample.getParagraphList()) {
+            toIndex = fromIndex + paragraph.getSentenceList().size();
+
+            sublist = allInOne.subList(fromIndex, toIndex);
+            fromIndex = toIndex;
+
+            paragraph.setSentenceList(sublist);
+        }
+    }
+
+    public static List<Sentence> getAllInOne(Text workSample) {
+        List<Sentence> allInOne = new ArrayList<>();
+        for (Paragraph paragraph : workSample.getParagraphList()) {
+            for (Sentence sentence : paragraph.getSentenceList()) {
+                allInOne.add(sentence);
+            }
+        }
+        return allInOne;
+    }
+
+    public static void swap(LinkedList<Word> exchange) {
+        Word first, last;
+        if ((first = exchange.pollFirst()) != null
+                && (last = exchange.pollLast()) != null) {
+
+            exchange.addFirst(last);
+            exchange.addLast(first);
+        }
+
+    }
+}
--- text-parsing/src/com/epam/text/service/impl/TextServiceImpl.java
+++ text-parsing/src/com/epam/text/service/impl/TextServiceImpl.java
@@ -5,52 +5,24 @@ import com.epam.text.entity.Sentence;
 import com.epam.text.entity.Text;
 import com.epam.text.entity.Word;
 import com.epam.text.service.TextService;
+import com.epam.text.service.helper.TextHelper;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class TextServiceImpl implements TextService {
-
-    private void swap(LinkedList<Word> exchange) {
-        Word first, last;
-        if ((first = exchange.pollFirst()) != null
-                && (last = exchange.pollLast()) != null) {
-
-            exchange.addFirst(last);
-            exchange.addLast(first);
-        }
-
-    }
-
     @Override
     public int doSentenceInWordIncreasing(Text workSample) {
-        List<Sentence> allTogether = new ArrayList<>();
+        List<Sentence> allInOne = TextHelper.getAllInOne(workSample);
 
-        for (Paragraph paragraph : workSample.getParagraphList()) {
-            for (Sentence sentence : paragraph.getSentenceList()) {
-                allTogether.add(sentence);
-            }
-        }
-
-        Collections.sort(allTogether, new Comparator<Sentence>() {
-            @Override
-            public int compare(Sentence o1, Sentence o2) {
-                int sizeOne = o1.getWordList().size();
-                int sizeTwo = o2.getWordList().size();
-                return Integer.compare(sizeOne, sizeTwo);
-            }
+        Collections.sort(allInOne, (o1, o2) -> {
+            int sizeOne = o1.getWordList().size();
+            int sizeTwo = o2.getWordList().size();
+            return Integer.compare(sizeOne, sizeTwo);
         });
-        int fromIndex = 0;
-        int toIndex;
-        List<Sentence> sublist;
 
-        for (Paragraph paragraph : workSample.getParagraphList()) {
-            toIndex = fromIndex + paragraph.getSentenceList().size();
-
-            sublist = allTogether.subList(fromIndex, toIndex);
-            fromIndex = toIndex;
-
-            paragraph.setSentenceList(sublist);
-        }
+        TextHelper.putInOrderAfterSort(workSample, allInOne);
 
         return 0;
     }
@@ -63,7 +35,7 @@ public class TextServiceImpl implements TextService {
         for (Paragraph paragraph : workSample.getParagraphList()) {
             for (Sentence sentence : paragraph.getSentenceList()) {
                 exchange = new LinkedList<>(sentence.getWordList());
-                swap(exchange);
+                TextHelper.swap(exchange);
                 sentence.setWordList(exchange);
             }
 
